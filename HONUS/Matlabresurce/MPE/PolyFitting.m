9f718848c0916aca170272f4a68c45e54b9983bd
InsungChoi <cis_1733@msn.com>
2016-02-11 22:48:36 +0900

    Revert "파일 복사"
    This reverts commit 8bb33bf165370f3b1b51d439311db5c3f98700fa.

--- HONUS/Matlabresurce/MPE/PolyFitting.m
+++
@@ -1,7 +0,0 @@
-function PolyFitting(FrequencyFitted, FrequencySampled, DataSampled, n)
-
-    global DataFitted
-
-    P1=polyfit(FrequencySampled,DataSampled,n);
-
-    DataFitted=Polyval(P1,FrequencyFitted);
--- HONUS/Matlabresurce/TMsourcefiles/TMInit.m
+++
@@ -1,7 +0,0 @@
-function TMInit()
-% elseif Travl1==7, T1=TMRigid(L1, Density1,FlowRes1,SFactor1,h1,VCL1,TCL1); thick1=L1; 
-global TM;
-global Result;
-
-TM = eye(2);
-Result = eye(2);
